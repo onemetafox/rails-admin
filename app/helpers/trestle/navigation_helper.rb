37c40bd6956a440d935852fed6cecfec2556106c
Dante Ika <danteikabuz@gmail.com>
2019-10-28 18:13:12 +0100

    use model_name in human_admin_name as default

--- app/helpers/trestle/navigation_helper.rb
+++ app/helpers/trestle/navigation_helper.rb
@@ -2,7 +2,7 @@ module Trestle
   module NavigationHelper
     def current_navigation_item?(item)
       return true if current_page?(item.path)
-      return true if defined?(admin) && admin.name == item.admin.name
+      return true if defined?(admin) && admin == item.admin
 
       false
     end
